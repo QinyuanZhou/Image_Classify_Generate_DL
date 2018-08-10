d8833e991146f4335120dc988e7c96b5c00ac128
qian wan <34510769+YibaYan@users.noreply.github.com>
2018-08-10 22:59:06 +0800

    add svd

--- KMeansImageCompression/PCA.py
+++ KMeansImageCompression/PCA.py
@@ -1,5 +1,6 @@
 __author__ = 'wanqian'
 import numpy as np
+from numpy.linalg import svd
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -15,11 +16,22 @@ pca = PCA(n_components=2).fit(dataset)
 data_new = pca.transform(dataset)
 
 fig1 = plt.figure()
-ax = fig1.add_subplot(1, 2, 1, projection='3d')
+ax = fig1.add_subplot(1, 3, 1, projection='3d')
 ax.scatter(dataset[:,0], dataset[:,1], dataset[:,2], c=label)
 ax.set_title('inital data')
 
-ax = fig1.add_subplot(1, 2, 2)
+ax = fig1.add_subplot(1, 3, 2)
 ax.scatter(data_new[:,0], data_new[:,1], c=label)
 ax.set_title('pca data')
+
+Sigma = np.dot(np.transpose(dataset), dataset)/(dataset.shape[0])
+U, S, V = svd(Sigma)
+Ureduce = U[:, 0:2]
+reduce_svd_data = np.dot(dataset, Ureduce)
+# ax = fig1.add_subplot(1, 3, 3, projection='3d')
+# ax.scatter(reduce_svd_data[:,0], reduce_svd_data[:,1], reduce_svd_data[:,2], c=label)
+ax = fig1.add_subplot(1, 3, 3)
+ax.scatter(reduce_svd_data[:,0], reduce_svd_data[:,1], c=label)
+ax.set_title('reduce svd data')
+
 plt.show()
