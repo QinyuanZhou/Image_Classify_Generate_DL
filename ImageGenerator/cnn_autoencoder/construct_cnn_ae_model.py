3e4d95d1e99da3a4dfc333d5c465ced5375877e8
qian wan <34510769+YibaYan@users.noreply.github.com>
2018-08-16 17:00:16 +0800

    ..

--- ImageGenerator/cnn_autoencoder/construct_cnn_ae_model.py
+++ ImageGenerator/cnn_autoencoder/construct_cnn_ae_model.py
@@ -18,90 +18,67 @@ class Conv_AE(object):
     def encoder(self):
         with tf.name_scope('Encoder_Layer'):
             with tf.name_scope('Conv_Layer1'):
-                conv1 = tf.nn.conv2d(self.X, filter=tf.Variable(
-                    tf.truncated_normal([3, 3, self.img_depth, 64], stddev=0.1, dtype=tf.float32), name='weight1'),
-                                     strides=self.c_stride, padding='SAME') # n x 96 x 96 x 63
-                relu1 = tf.nn.relu(conv1 + tf.Variable(tf.constant(0.1, shape=[64]), name='bias1'))
+                conv1 = tf.nn.conv2d(self.X, filter=tf.Variable(tf.truncated_normal([3, 3, self.img_depth, 64], stddev=0.1, dtype=tf.float32), name='weight1'), strides=self.c_stride, padding='SAME') # n x 96 x 96 x 63
+                relu1 = tf.nn.relu(conv1)
                 pool1 = tf.nn.max_pool(relu1, ksize=self.p_size, strides=self.p_stride, padding='SAME') # n x 48 x 48 x 64
 
             with tf.name_scope('Conv_Layer2'):
-                conv2 = tf.nn.conv2d(pool1, filter=tf.Variable(
-                    tf.truncated_normal([3, 3, 64, 32], stddev=0.1, dtype=tf.float32), name='weight2'),
-                                     strides=self.c_stride, padding='SAME') # n x 48 x 48 x 32
-                relu2 = tf.nn.relu(conv2 + tf.Variable(tf.constant(0.1, shape=[32]), name='bias2'))
+                conv2 = tf.nn.conv2d(pool1, filter=tf.Variable(tf.truncated_normal([3, 3, 64, 32], stddev=0.1, dtype=tf.float32), name='weight2'),strides=self.c_stride, padding='SAME') # n x 48 x 48 x 32
+                relu2 = tf.nn.relu(conv2)
                 pool2 = tf.nn.max_pool(relu2, ksize=self.p_size, strides=self.p_stride, padding='SAME') # n x 24 x 24 x 32
 
             with tf.name_scope('Conv_Layer3'):
-                conv3 = tf.nn.conv2d(pool2, filter=tf.Variable(
-                    tf.truncated_normal([3, 3, 32, 16], stddev=0.1, dtype=tf.float32), name='weight3'),
-                                     strides=self.c_stride, padding='SAME') # n x 24 x 24 x 16
-                relu3 = tf.nn.relu(conv3 + tf.Variable(tf.constant(0.1, shape=[16]), name='bias3'))
+                conv3 = tf.nn.conv2d(pool2, filter=tf.Variable(tf.truncated_normal([3, 3, 32, 16], stddev=0.1, dtype=tf.float32), name='weight3'), strides=self.c_stride, padding='SAME') # n x 24 x 24 x 16
+                relu3 = tf.nn.relu(conv3)
                 self.econ = tf.nn.max_pool(relu3, ksize=self.p_size, strides=self.p_stride, padding='SAME') # n x 12 x 12 x 16
-
-                p_mean, p_var = tf.nn.moments(self.econ, axes=[0, 1, 2], name='E_moments1')
-                scale = tf.Variable(tf.ones([1]))
-                shift = tf.Variable(tf.zeros([1]))
-                epsilon = 0.000001
-                self.econ = tf.nn.batch_normalization(self.econ, p_mean, p_var, scale, shift, epsilon, name='E_batch_normalization')
 
     def middle_code(self): # n x 12 x 12 x 16
         with tf.name_scope('Middle_Code'):
             with tf.name_scope('M_Layer1'):
-                m_conv1 = tf.nn.conv2d(self.econ, filter=tf.Variable(
-                    tf.truncated_normal([3, 3, 16, 16], stddev=0.1, dtype=tf.float32), name='M_weight1'),
-                                     strides=self.c_stride, padding='SAME') # n x 12 x 12 x 16
-                m_relu1 = tf.nn.relu(m_conv1 + tf.Variable(tf.constant(0.1, shape=[16]), name='M_bias1'))
+                m_conv1 = tf.nn.conv2d(self.econ, filter=tf.Variable(tf.truncated_normal([3, 3, 16, 16], stddev=0.1, dtype=tf.float32), name='M_weight1'), strides=self.c_stride, padding='SAME') # n x 12 x 12 x 16
+                m_relu1 = tf.nn.relu(m_conv1)
                 self.m_code = tf.nn.max_pool(m_relu1, ksize=self.p_size, strides=self.p_stride, padding='SAME') # n x 6 x 6 x 16
 
                 m_mean, m_var = tf.nn.moments(self.m_code, axes=[0, 1, 2], name='M_moments')
                 scale = tf.Variable(tf.ones([1]))
                 shift = tf.Variable(tf.zeros([1]))
                 epsilon = 0.000001
-                self.m_code = tf.nn.batch_normalization(self.m_code, m_mean, m_var, scale, shift, epsilon, name='M_batch_normalization')
+                self.m_code = tf.nn.batch_normalization(self.m_code, m_mean, m_var, scale, shift, epsilon)
 
     def decoder(self, rand_code): # n x 6 x 6 x 16
         with tf.name_scope('Decoder_Layer'):
             with tf.name_scope('Decoder_Layer1'):
                 d_conv1 = tf.image.resize_nearest_neighbor(rand_code, (12, 12)) # n x 12 x 12 x 16
-                d_conv1 = tf.nn.conv2d(d_conv1, filter=tf.Variable(
-                    tf.truncated_normal([3, 3, 16, 16], stddev=0.1, dtype=tf.float32), name='D_weight1'),
-                                     strides=self.c_stride, padding='SAME') # n x 12 x 12 x 16
-                d_conv1 = tf.nn.relu(d_conv1 + tf.Variable(tf.constant(0.1, shape=[16]), name='D_bias1'))
+                d_conv1 = tf.nn.conv2d(d_conv1, filter=tf.Variable(tf.truncated_normal([3, 3, 16, 16], stddev=0.1, dtype=tf.float32), name='D_weight1'), strides=self.c_stride, padding='SAME') # n x 12 x 12 x 16
+                d_conv1 = tf.nn.relu(d_conv1)
 
             with tf.name_scope('Decoder_Layer2'):
                 d_conv2 = tf.image.resize_nearest_neighbor(d_conv1, (24, 24)) # n x 24 x 24 x 16
-                d_conv2 = tf.nn.conv2d(d_conv2, filter=tf.Variable(
-                    tf.truncated_normal([3, 3, 16, 32], stddev=0.1, dtype=tf.float32), name='D_weight2'),
-                                     strides=self.c_stride, padding='SAME') # n x 24 x 24 x 32
-                d_conv2 = tf.nn.relu(d_conv2 + tf.Variable(tf.constant(0.1, shape=[32]), name='D_bias2'))
+                d_conv2 = tf.nn.conv2d(d_conv2, filter=tf.Variable(tf.truncated_normal([3, 3, 16, 32], stddev=0.1, dtype=tf.float32), name='D_weight2'), strides=self.c_stride, padding='SAME') # n x 24 x 24 x 32
+                d_conv2 = tf.nn.relu(d_conv2)
 
             with tf.name_scope('Decoder_Layer3'):
                 d_conv3 = tf.image.resize_nearest_neighbor(d_conv2, (48, 48)) # n x 48 x 48 x 32
-                d_conv3 = tf.nn.conv2d(d_conv3, filter=tf.Variable(
-                    tf.truncated_normal([3, 3, 32, 64], stddev=0.1, dtype=tf.float32), name='D_weight3'),
-                                     strides=self.c_stride, padding='SAME') # n x 48 x 48 x 64
-                d_conv3 = tf.nn.relu(d_conv3 + tf.Variable(tf.constant(0.1, shape=[64]), name='D_bias3'))
+                d_conv3 = tf.nn.conv2d(d_conv3, filter=tf.Variable(tf.truncated_normal([3, 3, 32, 64], stddev=0.1, dtype=tf.float32), name='D_weight3'), strides=self.c_stride, padding='SAME') # n x 48 x 48 x 64
+                d_conv3 = tf.nn.relu(d_conv3)
 
             with tf.name_scope('Decoder_Layer4'):
                 d_conv4 = tf.image.resize_nearest_neighbor(d_conv3, (96, 96)) # n x 96 x 96 x 32
-                recon = tf.nn.conv2d(d_conv4, filter=tf.Variable(tf.truncated_normal([3, 3, 64, 3], stddev=0.1,
-                                dtype=tf.float32), name='D_weight4'), strides=self.c_stride, padding='SAME') # n x 96 x 96 x 3
+                recon = tf.nn.conv2d(d_conv4, filter=tf.Variable(tf.truncated_normal([3, 3, 64, 3], stddev=0.1, dtype=tf.float32), name='D_weight4'), strides=self.c_stride, padding='SAME') # n x 96 x 96 x 3
         return recon
 
-    def train_net(self, sess, X):
-        feed_dict = {self.X: X}
-        self.encoder()
-        self.middle_code()
-        recon_image = self.decoder(self.m_code)
-        self.loss = tf.reduce_mean(tf.square(self.X - recon_image))
-        train = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
-
-        model_saver = tf.train.Saver()
-        init_var = tf.global_variables_initializer()
-        sess.run(init_var)
-        r, l, t = sess.run([recon_image, self.loss, train], feed_dict=feed_dict)
-
-        return r, l, t, model_saver
+    # def train_net(self, sess, X):
+    #     feed_dict = {self.X: X}
+    #     self.loss = tf.reduce_mean(tf.square(self.X - recon_image))
+    #     train = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+    #
+    #     model_saver = tf.train.Saver()
+    #     init_var = tf.global_variables_initializer()
+    #     sess.run(init_var)
+    #     # print('Activate train_net function')
+    #     r, l, t = sess.run([recon_image, self.loss, train], feed_dict=feed_dict)
+    #
+    #     return  r, l, t, model_saver
 
     def random_generate_image(self, sess, rand_code):
         feed_dict = {self.rand_code: rand_code}
--- ImageGenerator/cnn_autoencoder/train_cnn_ae.py
+++ ImageGenerator/cnn_autoencoder/train_cnn_ae.py
@@ -24,12 +24,21 @@ def next_batch(train_data, batch_size):
 if __name__ == '__main__':
 
     print('[INFO] loading images')
-    source_image = face_images[:256] # (51223, 96, 96, 3)
+    source_image = face_images[:4096] # (51223, 96, 96, 3)
+    source_image = np.array((source_image)) # (51223, 96, 96, 3)
+    print(source_image.shape)
     print('[INFO] Complete loading images')
 
-    batch_size = 64
-    epoch = 500
+    batch_size = 256
+    epoch = 5000
     COVN_AE = Conv_AE()
+    COVN_AE.encoder()
+    COVN_AE.middle_code()
+    recon_image = COVN_AE.decoder(COVN_AE.m_code)
+    loss = tf.reduce_mean(tf.square(COVN_AE.X - recon_image))
+    train = tf.train.AdamOptimizer(COVN_AE.learning_rate).minimize(loss)
+    model_saver = tf.train.Saver()
+    init_var = tf.global_variables_initializer()
 
     hist_loss = []
     fig1 = plt.figure('Loss')
@@ -37,29 +46,33 @@ if __name__ == '__main__':
     plt.ion()
 
     with tf.Session() as sess:
+        sess.run(init_var)
+
         for e in range(epoch):
             print('[INFO] epoch {} training, wait...'.format(e + 1))
             X_batches = next_batch(source_image, batch_size)
             all_batch = len(X_batches)
             for k, X in enumerate(X_batches):
-                if k == all_batch - 1:
-                    break
-                recon_image1, loss1, train1, saver = COVN_AE.train_net(sess, X)
+                # with tf.device('/gpu:0'):
+                feed_dict = {COVN_AE.X: X}
+                recon_image1, loss1, train1 = sess.run([recon_image, loss, train], feed_dict=feed_dict)
                 hist_loss.append(loss1)
                 plt.figure('Loss')
                 plt.plot(hist_loss)
-                print("[INFO] epoch/batch: {}/{}, loss {:.5f}".format(e + 1, k + 1, loss1))
+                print("[INFO] epoch/all_batch/batch: {}/{}/{}, loss {:.5f}".format(e + 1, all_batch, k + 1, loss1))
 
                 for i in range(5):
                     ax = fig2.add_subplot(2, 5, i+1)
-                    ax.imshow(np.reshape(X[i], [96, 96, 3]))
+                    init_image = cv2.cvtColor(np.reshape(X[i], [96, 96, 3]), cv2.COLOR_BGR2RGB)
+                    ax.imshow(init_image)
                     ax.axis('off')
-                    ax = fig2.add_subplot(2, 5, i + 5)
-                    ax.imshow(np.reshape(recon_image1[i], [96, 96, 3]))
+                    ax = fig2.add_subplot(2, 5, i + 6)
+                    re_image = cv2.cvtColor(np.reshape(recon_image1[i], [96, 96, 3], cv2.COLOR_BGR2RGB))
+                    ax.imshow(re_image)
                     ax.axis('off')
 
                 plt.pause(0.1)
-            saver.save(sess, "../model/COVN_AE.ckpt")
-        saver.save(sess, "../model/COVN_AE.ckpt")
+                model_saver.save(sess, "../model/COVN_AE.ckpt")
+            model_saver.save(sess, "../model/COVN_AE.ckpt")
     plt.ioff()  # close interactive mode
     plt.show()
